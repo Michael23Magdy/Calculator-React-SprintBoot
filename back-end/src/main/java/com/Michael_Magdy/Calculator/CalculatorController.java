8a0539c5663e81076d5226d209b2cc8cdcee1b97
Michael Magdy <michaelmagdymagdy@gmail.com>
2024-10-26 13:41:23 +0300

    added controller for expression calculation

---
+++ back-end/src/main/java/com/Michael_Magdy/Calculator/CalculatorController.java
@@ -0,0 +1,23 @@
+package com.Michael_Magdy.Calculator;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
+@RestController
+@RequestMapping("/api")
+public class CalculatorController {
+    @GetMapping("/calculate")
+    public ResponseEntity<Double> calculate(@RequestParam String expression){
+        try {
+            Expression e = new ExpressionBuilder(expression).build();
+            return new ResponseEntity<>(e.evaluate(), HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(0.0, HttpStatus.BAD_REQUEST);
+        }
+
+    }
+}
