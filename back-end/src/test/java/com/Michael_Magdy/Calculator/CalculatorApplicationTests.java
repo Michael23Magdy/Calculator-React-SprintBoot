ad51406f945bcc3cc4dc6ae0fdbfc1bbf2e48fdb
Michael Magdy <michaelmagdymagdy@gmail.com>
2024-10-26 13:40:58 +0300

    Spring-boot setup

---
+++ back-end/src/test/java/com/Michael_Magdy/Calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Michael_Magdy.Calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
